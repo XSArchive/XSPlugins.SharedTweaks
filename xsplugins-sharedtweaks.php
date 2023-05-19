a950c4f83aa4099ab3d4218631ba9bb49a62cc80
XerShade <jonathan.campbell.1992@hotmail.co.uk>
2023-05-19 14:32:51 -0400

    Create xsplugins-sharedtweaks.php

---
+++ xsplugins-sharedtweaks.php
@@ -0,0 +1,8 @@
+/*
+Plugin Name: XerShade's Plugns - Shared Tweaks
+Plugin URI: https://github.com/XerShade/XSPlugins.SharedTweaks
+Description: A wordpress plugin that contains commonly used website specific code tweaks.
+Author: XerShade
+Author URI: https://www.xershade.ca/
+Version: 1.0.0
+*/
